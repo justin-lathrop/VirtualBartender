c71320f7965aa298e9fe58a7b6833bde9c56a5fa
jelathro <jelathrop@gmail.com>
2012-11-03 17:46:15 +0000

    Added wiringPi to the mix and create a test program.

---
+++ Code/testGPIO.c
@@ -0,0 +1,33 @@
+#include <wiringPi.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(void){
+	int pin = 7;
+	
+	printf("Beginning GPIO test with wiringPi.");
+
+	// Check if pins are setup correctly
+	if(wiringPiSetup() == -1)
+		exit(1);
+
+	// Declare pins as inputs or outputs
+	pinMode(pin, OUTPUT);
+
+	// Begin main GPIO test program
+	for(;;){
+		printf(", Begin main program");
+
+		printf(", LED ON");
+		digitalWrite(pin, 1);
+
+		printf(", LED OFF");
+		digitalWrite(pin, 0);
+
+		delay(250);
+	}// for
+
+	return(0);
+}// main
